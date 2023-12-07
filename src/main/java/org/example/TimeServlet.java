ac7e9d0ede48e00aea40b6d7f1de1e83e97ed1a0
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2023-12-07 20:39:18 +0200

    init commit3

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -3,6 +3,7 @@ package org.example;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,7 +30,7 @@ public class TimeServlet extends HttpServlet {
         engine = new TemplateEngine();
         FileTemplateResolver resolver = new FileTemplateResolver();
 
-        resolver.setPrefix("C:/Users/Abramiuk/Documents/GitHub/GoIT_dev_homework_9/src/main/resources/templates/");
+        resolver.setPrefix(getClass().getResource("/templates/").getPath());
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(engine.getTemplateResolvers().size());
@@ -42,15 +43,13 @@ public class TimeServlet extends HttpServlet {
         resp.setContentType("text/html");
         Map<String, Object> params = new LinkedHashMap<>();
         String timezone = req.getParameter("timezone");
+        if(timezone!=null){
+            resp.addCookie(new Cookie("lastTimezone", timezone));
+        }
 
         if (timezone == null) {
-            if(req.getCookies() == null){
-                timezone = DEFAULT_TIMEZONE;
-            } else {
-                timezone = getTimezoneValue(req.getCookies());
-            }
+            timezone = getTimezoneValue(req.getCookies());
         }
-        resp.addCookie(new Cookie("lastTimezone", timezone));
 
         params.put("time" + timezone, getCurrentTime(timezone));
         Context context = new Context(req.getLocale(), Map.of("queryTime", params));
@@ -60,12 +59,14 @@ public class TimeServlet extends HttpServlet {
 
 
     private String getTimezoneValue(Cookie[] cookies) {
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("lastTimezone")) {
-                return cookie.getValue();
+        if(cookies!=null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("lastTimezone")) {
+                    return cookie.getValue();
+                }
             }
         }
-        return null;
+        return DEFAULT_TIMEZONE;
     }
 
     private String getCurrentTime(String timezone){
